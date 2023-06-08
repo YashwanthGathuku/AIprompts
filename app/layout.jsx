1f2f201f96f6bed6c8ddbdbea53eec3bf0cedb7e
Sphinxlike <yashwanth8035@gmail.com>
2023-06-08 14:03:38 -0400

    Navbar

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import '@styles/global.css';
+
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "AI Prompts",
+    description:'Discover & Share AI Prompts'
+}
+
+
+const Rootlayout = ({ children }) => {
+  return (
+    <html lang="en">
+        <body>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient' />
+                </div>
+
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
+        </body>
+    </html>
+  )
+}
+
+export default Rootlayout
