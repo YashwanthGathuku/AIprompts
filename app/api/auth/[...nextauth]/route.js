4f1ec21662e5c3606e007cfe3d359971fb1c9a78
Sphinxlike <yashwanth8035@gmail.com>
2023-06-08 15:14:19 -0400

    googleAPI

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,14 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+// console.log( {clientId: process.env.GOOGLE_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET})
+
 const handler = NextAuth({
     providers:[
         GoogleProvider({
-            clientId:'',
-            clientSecret:'',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
         })
     ],
@@ -14,7 +17,11 @@ const handler = NextAuth({
     },
 
     async signIn( { profile}){
+        try{
+            // serverless -> lambda-> dynamodb
+        } catch(error){
 
+        }
     }
 })
 
